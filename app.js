221ce60f4c2c2d4bd7fd54257b3f4d046a057875
Odkhuu <odkhuu@must.edu.mn>
2020-06-30 16:08:24 +0800

    finance module hiij ehellee. Expense, Incomes objectuudiig uusgelee.

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
 // Delgetstei ajillah controller
 var uiController = (function () {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: '.add__btn',
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            }
+        },
+        getDOMstrings : function() {
+            return DOMstrings;
+        }
+    }
 })();
 
 // Sankhuutei ajillah controller
 var financeController = (function () {
-    
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      }
+      
+      var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      }
+
+    //   var incomes = [];
+    //   var expenses = [];
+    //   var totalIncomes = [];
+    //   var totalExpenses = [];
+
+      var data ={
+          allItems: {
+              inc: [],
+              exp : []
+          },
+          totalItems: {
+            inc : 0,
+            exp : 0,
+        }
+      };
 })();
 
 // Eventtei ajillah controller
 var appController = (function (uiCtrl, financeCtrl) {
+    
     var ctrlAddItem = function () {
-        // 1. oruulah ugugdliig delgetsees olj avna
-        console.log("Delgetsees ugugdluu avah heseg");
+        // 1. oruulah ugugdliig delgetsees olj avna.
+
+        console.log(uiController.getInput());
+            
         
         // 2. Olj avsan ugugdluudee sanhuugiin controllert damjuulj tend hadgalna
 
@@ -24,20 +72,27 @@ var appController = (function (uiCtrl, financeCtrl) {
 
         // console.log('clicked');
     }
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        ctrlAddItem();
-    });
-    document.addEventListener('keypress', function(event) {
-        // console.log("Tovch daragdlaa" + event.keyCode);
-        if(event.keyCode === 13 || event.which === 13) {
+    var setupEventListeners = function () {
+
+        var DOM = uiCtrl.getDOMstrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click', function() {
             ctrlAddItem();
+        });
+        document.addEventListener('keypress', function(event) {
+            // console.log("Tovch daragdlaa" + event.keyCode);
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+    return {
+        init: function() {
+            console.log("programm started...");
+            setupEventListeners();
         }
-        // else console.log("Uur tovch darsan baina6" + event.keyCode);
-        
-        
-        
-    })
-})(
-    uiController, 
-    financeController
-    );
+    }
+})(uiController, financeController);
+
+appController.init();
